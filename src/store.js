ff4758badca6bb10b911b8c038baba421f292a8e
stanley <stanleyhuang83@gmail.com>
2020-04-29 17:41:34 -0700

    different file stores for dev and production

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore } from "redux";
 import reducer from "./reducers/reducer"
 const FileStore = require('electron-store');
-const fileStore = new FileStore();
+const options = (process.env.NODE_ENV == "development") ? {cwd: "dev"} : {}
+const fileStore = new FileStore(options);
 const initialState = fileStore.get("app_state");
-
 const store = createStore(reducer, initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 store.subscribe(() => {
